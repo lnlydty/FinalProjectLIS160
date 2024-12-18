f67bf1b9eb012de6e8c1979998e0cd21adf3663f
Alliah Mae <adhisuan@up.edu.ph>
2024-12-18 20:17:42 +0800

    host

--- dcrm2/settings.py
+++ dcrm2/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'lisdcrm2',
         'USER': 'root',
         'PASSWORD': 'DjangoPassword123',
-        'HOST': 'localhost', 
+        'HOST': '127.0.0.1', 
         'PORT': '3306',
     }
 }
